06374419abde27ca8ea4d0aab2327a9bbe753c86
yni <yn2443@columbia.edu>
2022-11-17 13:12:23 -0500

    oof hijacked; start over or abort;

--- server.py
+++ server.py
@@ -594,30 +594,30 @@ def directly_book():
     # query_con_1 = 'AND ({} IS NULL OR c.cruise_cost <= {})'.format(cust_budget_loc, cust_budget_loc ) # tested!
     # query_con_2 = 'AND ({} IS NULL OR c.cruise_rating >= {})'.format(cust_rating_loc, cust_rating_loc)  # tested!
     
-    # new solution: use if statement to modify the strings
-    query_con_1 = ""
-    query_con_2 = ""
-    query_con_3 = ""
-    query_con_4 = ""
-    query_con_5 = ""
+    # # new solution: use if statement to modify the strings
+    # query_con_1 = ""
+    # query_con_2 = ""
+    # query_con_3 = ""
+    # query_con_4 = ""
+    # query_con_5 = ""
 
-    if cust_budget_loc != "":
-      query_con_1 = "AND c.cruise_cost <= {} ".format(cust_budget_loc)
+    # if cust_budget_loc != "":
+    #   query_con_1 = "AND c.cruise_cost <= {} ".format(cust_budget_loc)
 
-    if cust_rating_loc != "":
-      query_con_2 = "AND c.cruise_rating >= {} ".format(cust_rating_loc)
+    # if cust_rating_loc != "":
+    #   query_con_2 = "AND c.cruise_rating >= {} ".format(cust_rating_loc)
 
-    if cust_specialty_loc != "":
-      query_con_3 = "AND (d1.dest_specialty = \"{}\" OR d2.dest_specialty = \"{}\" ) ".format(cust_specialty_loc, cust_specialty_loc)
+    # if cust_specialty_loc != "":
+    #   query_con_3 = "AND (d1.dest_specialty = \"{}\" OR d2.dest_specialty = \"{}\" ) ".format(cust_specialty_loc, cust_specialty_loc)
 
-    if cust_climate_loc != "":
-      query_con_4 = "AND (d1.dest_climate = \"{}\" OR d2.dest_climate = \"{}\" ) ".format(cust_climate_loc, cust_climate_loc)
+    # if cust_climate_loc != "":
+    #   query_con_4 = "AND (d1.dest_climate = \"{}\" OR d2.dest_climate = \"{}\" ) ".format(cust_climate_loc, cust_climate_loc)
     
-    if is_domestic_loc != "":
-      query_con_5 = "AND (d1.dest_is_domestic = \"{}\" AND d2.dest_is_domestic = \"{}\" ) ".format(is_domestic_loc, is_domestic_loc)
+    # if is_domestic_loc != "":
+    #   query_con_5 = "AND (d1.dest_is_domestic = \"{}\" AND d2.dest_is_domestic = \"{}\" ) ".format(is_domestic_loc, is_domestic_loc)
   
-    sql_query = query_select + query_from + query_whr + query_con_1 + query_con_2 + query_con_3 + query_con_4 + query_con_5
-    print(sql_query)
+    # sql_query = query_select + query_from + query_whr + query_con_1 + query_con_2 + query_con_3 + query_con_4 + query_con_5
+    # print(sql_query)
   
     cursor = g.conn.execute(sql_query)
     for result in cursor:
