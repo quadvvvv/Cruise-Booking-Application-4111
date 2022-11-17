60148d58d7f18130589e6dddac47f05ef2df2ce0
yni <yn2443@columbia.edu>
2022-11-17 15:39:44 -0500

    comment out  template codes; start final testing;

--- server.py
+++ server.py
@@ -89,98 +89,99 @@ def teardown_request(exception):
     pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-#
-# see for routing: https://flask.palletsprojects.com/en/2.0.x/quickstart/?highlight=routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
-@app.route('/index')
-def index():
-  """
-  request is a special object that Flask provides to access web request information:
+# #
+# # @app.route is a decorator around index() that means:
+# #   run index() whenever the user tries to access the "/" path using a GET request
+# #
+# # If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
+# #
+# #       @app.route("/foobar/", methods=["POST", "GET"])
+# #
+# # PROTIP: (the trailing / in the path is important)
+# #
+# # see for routing: https://flask.palletsprojects.com/en/2.0.x/quickstart/?highlight=routing
+# # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
+# #
+# @app.route('/index')
+# def index():
+#   """
+#   request is a special object that Flask provides to access web request information:
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+#   request.method:   "GET" or "POST"
+#   request.form:     if the browser submitted a form, this contains the data in the form
+#   request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
 
-  See its API: https://flask.palletsprojects.com/en/2.0.x/api/?highlight=incoming%20request%20data
+#   See its API: https://flask.palletsprojects.com/en/2.0.x/api/?highlight=incoming%20request%20data
 
-  """
+#   """
 
-  # DEBUG: this is debugging code to see what request looks like
-  print(request.args)
+#   # DEBUG: this is debugging code to see what request looks like
+#   print(request.args)
 
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
+#   #
+#   # example of a database query
+#   #
+#   cursor = g.conn.execute("SELECT name FROM test")
+#   names = []
+#   for result in cursor:
+#     names.append(result['name'])  # can also be accessed using result[0]
+#   cursor.close()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #
-  #     # creates a <div> tag for each element in data
-  #     # will print:
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+#   #
+#   # Flask uses Jinja templates, which is an extension to HTML where you can
+#   # pass data to a template and dynamically generate HTML based on the data
+#   # (you can think of it as simple PHP)
+#   # documentation: https://realpython.com/primer-on-jinja-templating/
+#   #
+#   # You can see an example template in templates/index.html
+#   #
+#   # context are the variables that are passed to the template.
+#   # for example, "data" key in the context variable defined below will be
+#   # accessible as a variable in index.html:
+#   #
+#   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+#   #     <div>{{data}}</div>
+#   #
+#   #     # creates a <div> tag for each element in data
+#   #     # will print:
+#   #     #
+#   #     #   <div>grace hopper</div>
+#   #     #   <div>alan turing</div>
+#   #     #   <div>ada lovelace</div>
+#   #     #
+#   #     {% for n in data %}
+#   #     <div>{{n}}</div>
+#   #     {% endfor %}
+#   #
+#   context = dict(data = names)
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
+#   #
+#   # render_template looks in the templates/ folder for files.
+#   # for example, the below file reads template/index.html
+#   #
+#   return render_template("index.html", **context)
 
-#
-# This is an example of a different path.  You can see it at:
-#
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("another.html")
+# #
+# # This is an example of a different path.  You can see it at:
+# #
+# #     localhost:8111/another
+# #
+# # Notice that the function name is another() rather than index()
+# # The functions for each app.route need to have different names
+# #
+# @app.route('/another')
+# def another():
+#   return render_template("another.html")
 
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
-  return redirect('/')
+# # Example of adding new data to the database
+# @app.route('/add', methods=['POST'])
+# def add():
+#   name = request.form['name']
+#   g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
+#   return redirect('/')
 
 ### Project Codes ###
+
 
 @app.route('/')
 def home_2():
@@ -568,7 +569,6 @@ def directly_book():
   is_domestic_loc = request.form['is_domestic']
 
   #SELECT s1.cruise_id, s1.dest_id AS to_dest, s2.dest_id AS from_dest
-  #
   
   try:
     if is_domestic_loc == "FALSE": # can be overseas
